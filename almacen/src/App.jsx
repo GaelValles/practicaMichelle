4d00363a7cf28eb58dec6e24d49c6c498f555694
Gael Emmanuel Valles Yañez <91341325+GaelValles@users.noreply.github.com>
2024-04-18 00:01:37 -0600

    prueba 6

--- almacen/src/App.jsx
+++ almacen/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Loginpage from './pages/Loginpage';
 import Registerpage from './pages/Registerpage';
-import Home from './pages/Home';
+
 import Insertproduct from './pages/Insertproduct';
 import Insertdealer from './pages/Insertdealer';
 import { AuthProvider } from './components/AuthContext';
@@ -10,7 +10,7 @@ function app() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Loginpage />} />
           <Route path="/login" element={<Loginpage />} />
           <Route path="/register" element={<Registerpage />} />
           <Route path="/insertproduct" element={<Insertproduct />} />
